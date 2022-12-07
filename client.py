113b998ea305ffdc433e2a350bba3569b080cf97
cYingy <2080396894@qq.com>
2022-12-07 16:10:56 +0800

    socket

---
+++ client.py
@@ -0,0 +1,70 @@
+import threading
+import socket
+
+my_name = "客户端"
+goal_name = "服务器"
+encoding = "utf-8"
+sever_ip = ""
+sever_port = None
+loop = 0
+
+
+def send_my(tcp_socket):
+    while True:
+        send_data = input(my_name + ":|")
+        print()
+        if send_data == "exit":
+            break
+        tcp_socket.send(send_data.encode(encoding))
+
+
+def rev_my(new_socket):
+    global loop
+    while True:
+        try:
+            rev_data = new_socket.recv(1024)
+        except ConnectionError as e:
+            print("\n服务器已断开连接")
+            loop = 1
+            break
+
+        print("\n")
+        print("%s:|%s".rjust(37, " ") % (rev_data.decode(encoding), goal_name))
+        print(my_name + ":|", end="")
+
+
+def menu():
+    global sever_ip, sever_port
+    sever_ip = input("请输入服务器的ip地址：")
+    sever_port = int(input("请输入服务器的端口："))
+    print("udp_socket客户端通信器".center(50, " "))
+    print("=" * 50)
+
+
+def main():
+    # 1.菜单提示
+    menu()
+    # 2.创建套接字
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 3.链接服务器
+    tcp_socket.connect((sever_ip, sever_port))
+    # while True:
+    # 6.创建子线程发送数据
+    send_t = threading.Thread(target=send_my, args=(tcp_socket,))
+    # 7.创建子线程接收数据
+    rev_t = threading.Thread(target=rev_my, args=(tcp_socket,))
+    # 8.线程开始
+    rev_t.start()
+    send_t.start()
+    # 9.关闭局部套接字
+    global loop
+    while loop == 0:
+        pass
+    loop = 0
+    tcp_socket.close()
+
+
+if __name__ == '__main__':
+    main()
+
+
